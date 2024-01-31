0ee848d6a11b6f905888da666c449234085adb47
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-31 01:13:26 +0000

    feat: Implemented NameCard()

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                  BusinessCardApp()
                 }
             }
         }
@@ -30,14 +42,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BusinessCardApp() {
+    NameCard(
+        imagePainter = painterResource(R.drawable.android_logo),
+        fullName = stringResource(R.string.full_name),
+        proffesion = stringResource(R.string.proffesion))
+
+}
+
+@Composable
+fun NameCard(
+    imagePainter: Painter,
+    fullName: String,
+    proffesion: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Image(
+            painter = imagePainter,
+            contentDescription = "Android logo",
+            modifier = Modifier
+                .background(Color.Black)
+                .size(100.dp)
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = fullName,
+            fontWeight = FontWeight.Light,
+            fontSize = 40.sp,
+            fontStyle = FontStyle.Normal,
+            fontFamily = FontFamily.SansSerif
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = proffesion,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            color = Color(0xFF3ddc84)
+        )
+    }
+
+}
+
+@Composable
+fun DetailsCard() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+       BusinessCardApp()
     }
 }
